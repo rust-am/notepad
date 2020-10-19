de57a8d3b92a4120219e9fd2355d02d83f7aa0f9
rustam023 <r.91@mail.ru>
2020-10-19 16:58:08 +0300

    notepad v1.0

---
+++ lib/link.rb
@@ -0,0 +1,22 @@
+class Link < Post
+
+  def initialize
+    super
+
+    @url = ''
+  end
+
+  def read_from_console
+    puts 'Адрес ссылки (url):'
+    @url = STDIN.gets.chomp
+
+    puts 'Что за ссылка?'
+    @text = STDIN.gets.chomp
+  end
+
+  def to_strings
+    time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')} \n"
+
+    [@url, @text, time_string]
+  end
+end
--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,16 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
-    @text = nil
+    @text = []
   end
 
   def read_from_console
